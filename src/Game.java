5ea0d66e817762422d9c5c43f83814f0d8ede33c
jagger1407 <jagger.pib@gmail.com>
2025-07-16 17:26:35 +0200

    Game - add user input handling

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
 
 public class Game {
 
 	public static void main(String[] args) {
-		Board b = new Board();
-		System.out.println(b.toString());
+		Board b = null;
+		Scanner input = new Scanner(System.in);
+		
+		boolean running = true;
+		boolean ingame = false;
+		while(running) {
+			System.out.print("> ");
+			String inStr = input.nextLine();
+			if(inStr.equals("start")) {
+				ingame = true;
+				b = new Board();
+				System.out.println("Board initialized.");
+			}
+			else if(inStr.equals("board")) {
+				if(!ingame) {
+					System.out.println("No game running.");
+					continue;
+				}
+				System.out.println(b.toString());
+			}
+			else if(inStr.equals("exit") || inStr.equals("quit") || 
+					inStr.equals("end") || inStr.equals("stop")) {
+				running = false;
+			}
+		}
+		
+		input.close();
 	}
 
 }
